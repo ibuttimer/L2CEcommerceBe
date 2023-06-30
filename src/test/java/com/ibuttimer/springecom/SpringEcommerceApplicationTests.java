50f80bf96ced1a573d8b50e4992356115a9c6d98
Ian Buttimer <ibuttimer@yahoo.com>
2023-06-30 10:03:14 +0100

    fix: unit tests

--- src/test/java/com/ibuttimer/springecom/SpringEcommerceApplicationTests.java
+++ src/test/java/com/ibuttimer/springecom/SpringEcommerceApplicationTests.java
@@ -2,9 +2,14 @@ package com.ibuttimer.springecom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class SpringEcommerceApplicationTests {
+
+	@MockBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
